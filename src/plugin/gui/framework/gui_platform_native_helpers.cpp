721cc826cf922dfff11785f4b083845d1dd69c02
Sam Windell <info@frozenplain.com>
2024-08-03 10:58:01 +0100

    fix missing symbol

--- src/plugin/gui/framework/gui_platform_native_helpers.cpp
+++ src/plugin/gui/framework/gui_platform_native_helpers.cpp
@@ -1,12 +1,18 @@
 // Copyright 2018-2024 Sam Windell
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#ifdef __linux__
-#include <X11/Xlib.h>
 #include <pugl/pugl.h>
 
+#ifdef __linux__
+#include <X11/Xlib.h>
+#endif
+
 int FdFromPuglWorld(PuglWorld* world) {
+#ifdef __linux__
     auto display = (Display*)puglGetNativeWorld(world);
     return ConnectionNumber(display);
-}
+#else
+    (void)world;
+    return 0;
 #endif
+}
